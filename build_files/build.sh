6150fb67ef35cc75800871d6bda8f44df39a0d8f
findz8 <c.findlay14@outlook.com>
2026-03-04 20:49:22 +0000

    remove bazzite packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,11 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+# remove packages
+dnf remove -y Waydroid \
+              Sunwshine
+
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
